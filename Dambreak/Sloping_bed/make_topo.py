8c120abb9af6fde40396b9fa5443b1f3f107d0fb
Christophe Ancey <christophe.ancey@epfl.ch>
2024-02-10 11:07:28 +0100

    Add files via upload

---
+++ Dambreak/Sloping_bed/make_topo.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+xlower = 0
+xupper = 1000.
+slope  = 0.05
+mx     = 2000
+
+x = np.linspace(xlower, xupper, mx+1)
+z = 0-slope*x
+ 
+
+fname='topography.data'
+
+with open(fname,'w') as f:
+        f.write('%i   # number of points\n' % (mx+1))
+
+        for i in range(mx+1):
+            f.write('%15.8f %15.8f\n' % (x[i], z[i]))
+        f.close()
